f71704539c62f8345c71dc11abfe20f9e311211c
Isacs Macbook <Iwandiba@yahoo.com>
2024-03-01 09:26:01 -0500

    Fixed, frame rate bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,60 @@
 #include <SFML/Graphics.hpp>
 #include "Application.h"
 
+
+
+
+const float test = 1.0 / 60.0f; 
 int main()
 {
 
 
 
     sf::RenderWindow window(sf::VideoMode(700,700), "Cloth Sim");
+    window.setFramerateLimit(60);
     Application app(window);
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
 
-    
+
+
+    app.Setup(2,2,2);
+
+
+
+
     sf::Clock clock;
+    sf::Time elapsedTime = sf::Time::Zero;
+    const sf::Time timePerFrame = sf::seconds(1.0f / 60.0f); 
+    float dt; 
+
 
 
     while (window.isOpen())
     {
-      // sf::time Elasped = clock.restart();
+    //sf::Time Elasped = clock.getElapsedTime();
 
-        app.Setup(2,2,2);
-  
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+      sf::Time delta = clock.restart();
+      elapsedTime += delta;
 
+      while (elapsedTime >= timePerFrame)
+    {
+        app.Update(elapsedTime.asSeconds()); // Use the fixed timestep
+        elapsedTime -= timePerFrame;
+    }
+
+       // app.Update(elapsedTime.asSeconds());
         window.clear();
-        app.Update(clock);
         app.Render();
         window.display();
+ 
     }
 
     return 0;
