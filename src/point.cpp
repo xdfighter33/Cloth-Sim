de31fa6f704899dede8826f806960ea354cff060
Isacs Macbook <Iwandiba@yahoo.com>
2024-03-01 09:25:06 -0500

    Add debug commands and add forces

--- src/point.cpp
+++ src/point.cpp
@@ -5,32 +5,96 @@
 
 
 void Point::update_Point(float dt,float subSteps){
-sf::Vector2f grav{0,200.0f};
-float m_time;
-float step_dt = dt / subSteps;
 
-//step_dt += dt / 60;
-uint32_t m_subStep = (uint32_t)60;
+ uint32_t numSubsteps = calculateNumSubsteps(dt);    
+shit.setSubsStepscount(10);
+shit.setSimulationUpdateRate(60);
+shit.update();
 
-for(int i = 0; i < subSteps; i++ ) {
-    ApplyGravity(grav);
+float step_dt = shit.getStepDt();
+
+
+
+
+// Declare Variables; 
+debug_variables(step_dt);
+
+
+
+sf::Vector2f grav{0,20.0f};
+
+
+uint32_t tittys = shit.return_sub_steps();
+
+
+//Debug Test 
+
+
+set_velocity(sf::Vector2f(5,0),step_dt);
+
+for(uint32_t i(shit.return_sub_steps()); i--;) {
+  //  ApplyGravity(grav);
+  // setVelo(sf::Vector2f(2,0));
+  // setAccel(sf::Vector2f(5,800.0f ));
+  // appplyConstraint(step_dt);
     update_positions(step_dt);
 }
 
 }
 
+void Point::setAccel(sf::Vector2f force){
+    for(auto& obj: points){
 
+        shit.addAccerlation(obj,force);
+    }
+
+}
 void Point::set_step_dt(float rate){
 
 }
 
-void Point::update_Objects(float dt,float subSteps){
+void Point::setVelo(sf::Vector2f velo){
+
+    for(auto& obj: points){
+
+        
+    // 
+
+    }
+
+}
+
+void Point::debug_variables(float dt){
+
+std::cout << "M Frame: "  <<shit.return_m_frame() << std::endl;
+std::cout << "M time: " << shit.return_time() << std::endl; 
+std::cout << "Step DT: " << shit.getStepDt() << std::endl;
+std::cout << "Sub Step: " << shit.return_sub_steps() << std::endl;
+Velo(dt);
 
 
-    //dt += 1.0f / 
+
+}
+
+void Point::Velo(float dt ){
+    sf::Vector2f VELO;
     for(auto& obj : points){
-        obj.set_Update_Rate(60);
+      VELO.x =  obj.getVelocity(dt).x;
+      VELO.y =  obj.getVelocity(dt).y;
+    }
 
+
+
+std::cout << "Velocity: x " << VELO.x << "\n" << "Veloicty y: "<< VELO.y  << std::endl;
+
+}
+uint32_t Point::calculateNumSubsteps(float dt) {
+    // Adjust the number of substeps based on elapsed time
+    // You can define your own criteria for adjusting substeps
+    if (dt < 0.016f) { // Example threshold (corresponds to ~60 FPS)
+        return 1; // Default substeps
+    } else {
+        return static_cast<uint32_t>(dt / 0.016f); // Adjust substeps based on elapsed time
     }
 }
 
--- src/point.h
+++ src/point.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "physics/Physics.hpp"
-
+#include "physics/Sim.hpp"
 
 class Point{
 
@@ -9,6 +9,7 @@ public:
     Point() = default;
     Point(sf::Vector2f Position){
         Position = pos;
+        shit.setSimulationUpdateRate(60);
     }
     ~Point() = default;
 
@@ -27,16 +28,35 @@ void set_velocity(sf::Vector2f v,float dt){
 };
 
 
+void return_pos(){
+    float x,y;
+    for(auto& obj : points){
+       x =  obj.Get_Pos().x;
+       y =  obj.Get_Pos().y;
+    }
+
+    std::cout << "Objects Position is at " << x << std::endl;; 
+    std::cout << "Object y Position is at " << y << std::endl; 
+}
+
+
 void set_step_dt(float rate);
 const std::vector<Particle>& getObjects() const {
     return points;
 }
+void debug_variables(float dt);
+
+
+
+void Velo(float dt);
+
+uint32_t calculateNumSubsteps(float dt);
+
 
 void update_positions(float dt){
 
     for(auto& obj : points){
-        obj.updatePosition(dt);
-     //   obj.set_Velo(sf::Vector2f(50,50),dt);
+        shit.updateObject(obj,dt);
     }
 }
 
@@ -48,7 +68,7 @@ Particle& addObject(){
 }
 
 
-void update_Objects(float dt,float subSteps);
+void update_positions(float dt,float subSteps);
 
 void update_Point(float dt,float subSteps);
 
@@ -61,10 +81,48 @@ void ApplyGravity(sf::Vector2f grav){
 }
 
 
+void setVelo(sf::Vector2f);
+
+
+void setAccel(sf::Vector2f force);
+
+
+
+void appplyConstraint(float dt)
+{
+    for (auto& obj : points){
+        const uint64_t object_count = points.size();
+
+        for (uint64_t i{0}; i < object_count; i++)
+        {
+            Particle& object = points[i];
+            if(object.Get_Pos().y >= 200)
+            {
+                object.pos.y += -1;
+            }
+            if(object.Get_Pos().y <= 0)
+            {
+                object.pos.y += 1;
+            }
+             if(object.Get_Pos().x >= 250)
+            {
+                object.pos.x += -1;
+            }
+             if(object.Get_Pos().x <= 25)
+            {
+                object.pos.x += 1;
+            }
+        }
+    }
+}
+
+
+
+
 
 private:
 std::vector<Particle> points;
-
+Simulator shit;
 
 
 float size = 150.0f;
@@ -76,3 +134,5 @@ sf::Vector2f pos;
 
     
 };
+
+
